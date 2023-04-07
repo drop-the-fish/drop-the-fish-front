c8fa68dab2176fe33350e67deca9fd1c0f5b0566
hongjk7777 <hongjk7777@naver.com>
2023-04-07 14:52:49 +0900

    Chore: intial setting

---
+++ android/app/src/main/kotlin/com/example/drop_the_fish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drop_the_fish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:camera/camera.dart';
+import 'package:drop_the_fish/widgets/fish_bottom_navigation_bar.dart';
+import 'package:drop_the_fish/widgets/camera_screen_tab_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '하위'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
